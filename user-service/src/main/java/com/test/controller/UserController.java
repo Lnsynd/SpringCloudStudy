d7d0b728953baa511fb2fcb371a5a068c37438aa
lqs <2034215871@qq.com>
2022-11-30 20:45:44 +0800

    Nacos服务端注册 集群分区 配置中心 命名空间

--- user-service/src/main/java/com/test/controller/UserController.java
+++ user-service/src/main/java/com/test/controller/UserController.java
@@ -2,6 +2,8 @@ package com.test.controller;
 
 import com.test.entity.User;
 import com.test.service.UserService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,13 +11,22 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.Resource;
 
 @RestController
+@RefreshScope // 该注解实现实时刷新
 public class UserController {
     @Resource
     UserService service;
 
+    @Value("${test.lbwnb}")
+    String test;
+
+
     //这里以RESTFul风格为例
     @RequestMapping("/user/{uid}")
     public User findUserById(@PathVariable("uid") int uid){
+        System.out.println(test);
         return service.getUserById(uid);
     }
+
+
+
 }
